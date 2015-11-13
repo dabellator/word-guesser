db2b344cbcb3a0ebc7b154272fb503d4ec9e6f6b
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-11-12 23:54:00 -0800

    Changed readme

--- module.js
+++
@@ -1,6 +0,0 @@
-// This is example of using random.word.js module
-var word = require(__dirname + '/lib/random_word.js');
-
-word.getSimple(); // Just call the function in get request
-word.getHarder();
-
