6867d89869eba0c79363ec3a6e8cc541bcedc29d
walter <walternicholas@gmail.com>
2015-11-20 10:45:21 -0800

    added game test

--- test/game_test.js
+++ test/game_test.js
@@ -7,40 +7,44 @@ describe('The game module', function() {
 	it('should send back the proper feedback to an incorrect guess', function() {
 	    var answer = 'cat';
 	    var guess = 'dog';
+	    var expectedArray = ['d0','o0','g0'];
 	    var response = game(answer, guess);
 
 	    expect(response).to.exist;
  	    expect(response.gameOver).to.eql(false);
- 	    expect(response.arr).to.eql(['d0','o0','g0']);
+ 	    expect(response.arr).to.eql(expectedArray);
 	});	
 
 	it('should send back the proper feedback to another incorrect guess', function() {
 	    var answer = 'cat';
 	    var guess = 'tap';
+	    var expectedArray = ['t1','a2','p0'];
 	    var response = game(answer, guess);
 
 	    expect(response).to.exist;
  	    expect(response.gameOver).to.eql(false);
- 	    expect(response.arr).to.eql(['t1','a2','p0']);
+ 	    expect(response.arr).to.eql(expectedArray);
 	});
 
 	it('should send back the proper feedback to a winning guess', function() {
 	    var answer = 'cat';
 	    var guess = 'cat';
+	    var expectedArray = ['c2','a2','t2'];
 	    var response = game(answer, guess);
 
 	    expect(response).to.exist;
  	    expect(response.gameOver).to.eql(true);
- 	    expect(response.arr).to.eql(['c2','a2','t2']);
+ 	    expect(response.arr).to.eql(expectedArray);
 	});
 
 	it('should still send back the proper feedback to a guess and answer which both have multiple repeated letters', function() {
 	    var answer = 'bookkeeper';
 	    var guess = 'bbookkeepr';
+ 	    var expectedArray = ['b2','b0','o2', 'o1', 'k2', 'k1', 'e2', 'e1', 'p1', 'r2'];
 	    var response = game(answer, guess);
 
 	    expect(response).to.exist;
  	    expect(response.gameOver).to.eql(false);
- 	    expect(response.arr).to.eql(['b2','b0','o2', 'o1', 'k2', 'k1', 'e2', 'e1', 'p1', 'r2']);
+ 	    expect(response.arr).to.eql(expectedArray);
 	});
 });
