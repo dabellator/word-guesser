bc70aa833179af6731a49db009df88284b18c6df
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-11-19 18:23:04 -0800

    Small fix in UserSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -36,7 +36,7 @@ userSchema.methods.checkPassword = function(password) {
 userSchema.statics.updateUser = function (dataObject, cb) {
   var newTime = Math.round((dataObject.timeEnd - dataObject.timeStart)/1000);
   var newInfo = {word: dataObject.word, time: newTime, guesses: dataObject.guesses};
-  this.findOneAndUpdate({username: dataObject.username}, {$push: {stat: newInfo}}, {safe: true, upsert: true}).exec(cb);
+  this.findOneAndUpdate({username: dataObject.username}, {$push: {stat: newInfo}}, {safe: true, upsert: true}).exec();
   this.findOne({username: dataObject.username}, function (err, userObject) {
     if (err) return console.log(err);
     var newAvgTime = average(userObject.total.avg_time, userObject.total.guessed, newTime);
